e9349f534ece35a36967655b81236befb1f80a90
Olenteva74 <olenteva74@ukr.net>
2023-07-04 21:53:59 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,7 +13,7 @@ const auth = async(req, res, next) => {
         }
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        if (!user || !user.token || user.token !== token) {
+        if (!user.token) {
             throw new Unauthorized("Not authorized");
         }
         req.user = user;
